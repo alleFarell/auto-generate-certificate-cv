6bb7a072efdc3c4271db61433d139ded64c7ad4e
ghinakh <ghinak@student.telkomuniversity.ac.id>
2021-09-04 20:50:55 +0700

    update front end cv

--- resources/views/content/cv.blade.php
+++ resources/views/content/cv.blade.php
@@ -5,10 +5,9 @@
 <!-- DataTales Example -->
 <div class="container">
     <div class="d-flex justify-content-end mb-3">
-        <a class="btn btn-dark ml-2" href="{{ url('/cv/download_cv2') }}" target="_blank" style="background-color: #060239 !important">Dark Blue</a>
-        <a class="btn btn-light ml-2" href="{{ url('/cv/download_cv_rev') }}" target="_blank">White</a>
-        <a class="btn btn-dark ml-2" href="{{ url('/cv/download_cv_dark') }}" target="_blank" style="background-color: #2D2D2D !important">Dark</a>
-        <a class="btn btn-warning ml-2" href="{{ url('/cv/download_cv_grid') }}" target="_blank" style="">Grid</a>
+        <a class="btn btn-light ml-2" href="{{ url('/cv/download_cv_1') }}" target="_blank">White</a>
+        <a class="btn btn-dark ml-2" href="{{ url('/cv/download_cv_2') }}" target="_blank" style="background-color: #2D2D2D !important">Blue-Green</a>
+        <a class="btn btn-warning ml-2" href="{{ url('/cv/download_cv_3') }}" target="_blank" style="">Grid</a>
     </div>
 </div>
 
